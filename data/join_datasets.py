f7437eb8873c446fcd735979c9b4f8e0973a35a9
wrosinski <wojtusior@gmail.com>
2019-03-08 17:31:41 +0100

    join parquets

--- data/join_datasets.py
+++ data/join_datasets.py
@@ -2,12 +2,15 @@
 import numpy as np
 import pandas as pd
 
-train = pd.read_csv("train.csv")
+train = pq.read_table('./train.parquet').to_pandas()
 train["src"] = "train"
 train["is_test"] = 0
-test = pd.read_csv("test.csv")
+test = pq.read_table('./test.parquet').to_pandas()
 test["src"] = "test"
 test["is_test"] = (test["reference"].isnull() & (test["action_type"] == "clickout item")).astype(np.int)
 events = pd.concat([train, test], axis=0)
 events.sort_values(["timestamp", "user_id", "step"], inplace=True)
-events.to_csv("events_sorted.csv", index=False)
+
+table = pa.Table.from_pandas(events)
+pq.write_table(table, './events_sorted.parquet')
+print('events saved.')
