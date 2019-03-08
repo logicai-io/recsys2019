c468b14e1a792f43e7de6eb99cb08965364d8d75
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2019-03-08 16:39:45 +0100

    Validation: 0.4492 Leaderboard 0.5185

--- src/recsys/generate_training_data.py
+++ src/recsys/generate_training_data.py
@@ -1,171 +1,153 @@
 from collections import defaultdict
-from multiprocessing.pool import Pool
+from csv import DictReader, DictWriter
 
-import numpy as np
-import pandas as pd
-from lightgbm import LGBMClassifier
-from sklearn.compose import ColumnTransformer
-from sklearn.ensemble import VotingClassifier
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.impute import SimpleImputer
-from sklearn.metrics import roc_auc_score
-from sklearn.neural_network import MLPClassifier
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from tqdm import tqdm
 
 
-def calculate_mean_rec_err(queries, clicks):
-    recs = []
-    for click, query in zip(clicks, queries):
-        if query and click in query:
-            rec = 1 / (query.index(click) + 1)
-            recs.append(rec)
-    return np.array(recs).mean()
+class StatsAcc():
+    """
+    Example definition
+
+    StatsAcc(filter=lambda row: row.action_type == "clickout_item",
+             init_acc=defaultdict(int),
+             updater=lambda acc, row: acc[(row.user_id, row.item_id)]+=1)
+    """
+
+    def __init__(self, name, filter, acc, updater, get_stats_func):
+        self.name = name
+        self.filter = filter
+        self.acc = acc
+        self.updater = updater
+        self.get_stats_func = get_stats_func
+
+    def filter(self, row):
+        return self.filter(row)
+
+    def update_acc(self, row):
+        if self.filter(row):
+            self.updater(self.acc, row)
+
+    def get_stats(self, row, item):
+        return self.get_stats_func(self.acc, row, item_id)
 
 
-df = pd.read_csv("../../data/train_sample_1.csv")
-df.sort_values(["timestamp", "user_id"], inplace=True)
-df["id"] = np.arange(1, df.shape[0] + 1)
-
-item_metadata = pd.read_csv("../../data/item_metadata.csv", dtype={"item_id": str})
+def increment_key_by_one(acc, key):
+    acc[key] += 1
+    return acc
 
 
-# get max timestamp for clickouts
-# this extracts user_id and the timestamp of the last checkout
-df["num_action_per_user"] = df.groupby(("user_id", "action_type")).cumcount(ascending=False).values
-df["validation"] = (df["num_action_per_user"] == 0) & (df["action_type"] == "clickout item")
+def increment_keys_by_one(acc, keys):
+    for key in keys:
+        acc[key] += 1
+    return acc
 
-# keeps track of item CTR
-print("Building CTR stats")
-impressions_per_item = defaultdict(int)
-clicks_per_item = defaultdict(int)
 
-impressions_per_item_user = defaultdict(int)
-clicks_per_item_user = defaultdict(int)
+def set_key(acc, key, value):
+    acc[key] = value
 
-impressions_cum = {}
-clicks_cum = {}
 
-impressions_user_cum = {}
-clicks_user_cum = {}
+accumulators = [
+    StatsAcc(
+        name="clickout_user_item_clicks",
+        filter=lambda row: row["action_type"] == "clickout item",
+        acc=defaultdict(int),
+        updater=lambda acc, row: increment_key_by_one(acc, (row["user_id"], row["reference"])),
+        get_stats_func=lambda acc, row, item: acc[(row["user_id"], item)]
+    ),
+    StatsAcc(
+        name="clickout_item_clicks",
+        filter=lambda row: row["action_type"] == "clickout item",
+        acc=defaultdict(int),
+        updater=lambda acc, row: increment_key_by_one(acc, (row["reference"])),
+        get_stats_func=lambda acc, row, item: acc[item]
+    ),
+    StatsAcc(
+        name="clickout_item_impressions",
+        filter=lambda row: row["action_type"] == "clickout item",
+        acc=defaultdict(int),
+        updater=lambda acc, row: increment_keys_by_one(acc, row["impressions"].split("|")),
+        get_stats_func=lambda acc, row, item: acc[item]
+    ),
+    StatsAcc(
+        name="was_interaction_img",
+        filter=lambda row: row["action_type"] == "interaction item image",
+        acc={},
+        updater=lambda acc, row: set_key(acc, row["user_id"], row["reference"]),
+        get_stats_func=lambda acc, row, item: int(acc.get(row["user_id"]) == item)
+    ),
+    StatsAcc(
+        name="interaction_img_freq",
+        filter=lambda row: row["action_type"] == "interaction item image",
+        acc=defaultdict(int),
+        updater=lambda acc, row: increment_key_by_one(acc, (row["user_id"], row["reference"])),
+        get_stats_func=lambda acc, row, item: acc[(row["user_id"], item)]
+    ),
+    StatsAcc(
+        name="was_interaction_deal",
+        filter=lambda row: row["action_type"] == "interaction item deals",
+        acc={},
+        updater=lambda acc, row: set_key(acc, row["user_id"], row["reference"]),
+        get_stats_func=lambda acc, row, item: int(acc.get(row["user_id"]) == item)
+    ),
+    StatsAcc(
+        name="interaction_deal_freq",
+        filter=lambda row: row["action_type"] == "interaction item deals",
+        acc=defaultdict(int),
+        updater=lambda acc, row: increment_key_by_one(acc, (row["user_id"], row["reference"])),
+        get_stats_func=lambda acc, row, item: acc[(row["user_id"], item)]
+    ),
+    StatsAcc(
+        name="was_interaction_info",
+        filter=lambda row: row["action_type"] == "interaction item info",
+        acc={},
+        updater=lambda acc, row: set_key(acc, row["user_id"], row["reference"]),
+        get_stats_func=lambda acc, row, item: int(acc.get(row["user_id"]) == item)
+    ),
+    StatsAcc(
+        name="interaction_info_freq",
+        filter=lambda row: row["action_type"] == "interaction item info",
+        acc=defaultdict(int),
+        updater=lambda acc, row: increment_key_by_one(acc, (row["user_id"], row["reference"])),
+        get_stats_func=lambda acc, row, item: acc[(row["user_id"], item)]
+    )
+]
 
-for index, row in tqdm(df.iterrows()):
-    if row["action_type"] == "clickout item":
-        clicked_id = row["reference"]
+if __name__ == '__main__':
+    inp = open("../../data/events_sorted.csv")
+    dr = DictReader(inp)
+    out = open("../../data/events_sorted_trans.csv", "wt")
+    # keeps track of item CTR
+    all_obs = []
+    first_row = True
+    for clickout_id, row in enumerate(tqdm(dr)):
         user_id = row["user_id"]
-        impressions = row["impressions"].split("|")
+        if row["action_type"] == "clickout item":
+            item_ids = row["impressions"].split("|")
+            prices = list(map(int, row["prices"].split("|")))
+            # create training data
+            for rank, (item_id, price) in enumerate(zip(item_ids, prices)):
+                obs = row.copy()
+                del obs["impressions"]
+                del obs["prices"]
+                del obs["action_type"]
+                obs["item_id"] = item_id
+                obs["item_id_clicked"] = row["reference"]
+                obs["was_clicked"] = int(row["reference"] == item_id)
+                obs["clickout_id"] = clickout_id
+                obs["rank"] = rank
+                obs["price"] = price
+                for acc in accumulators:
+                    obs[acc.name] = acc.get_stats(row, item_id)
 
-        # track global impressions and clicks
-        impressions_stats = [impressions_per_item[i] for i in impressions]
-        impressions_cum[index] = ("|".join(map(str, impressions_stats)))
+                if first_row:
+                    dw = DictWriter(out, fieldnames=obs.keys())
+                    dw.writeheader()
+                    first_row = False
 
-        clicks_stats = [clicks_per_item[i] for i in impressions]
-        clicks_cum[index] = ("|".join(map(str, clicks_stats)))
+                dw.writerow(obs)
 
-        for item_id in impressions:
-            impressions_per_item[item_id] += 1
-        clicks_per_item[clicked_id] += 1
+        for acc in accumulators:
+            acc.update_acc(row)
 
-        # track user clicks set
-        impressions_stats_per_user = [impressions_per_item_user[(i, user_id)] for i in impressions]
-        impressions_user_cum[index] = ("|".join(map(str, impressions_stats_per_user)))
-
-        clicks_stats_per_user = [clicks_per_item_user[(i, user_id)] for i in impressions]
-        clicks_user_cum[index] = ("|".join(map(str, clicks_stats_per_user)))
-
-        for item_id in impressions:
-            impressions_per_item_user[(item_id, user_id)] += 1
-        clicks_per_item_user[(clicked_id, user_id)] += 1
-
-
-df["impressions_cum_for_item"] = df.index.map(impressions_cum)
-df["clicks_cum_for_item"] = df.index.map(clicks_cum)
-df["impressions_cum_for_item_user"] = df.index.map(impressions_user_cum)
-df["clicks_cum_for_item_user"] = df.index.map(clicks_user_cum)
-
-clickouts = df[df["action_type"] == "clickout item"]
-
-# benchmark
-print("Benchmark")
-print(calculate_mean_rec_err(clickouts["impressions"].map(lambda x: x.split("|")).to_list(), clickouts["reference"]))
-
-dfs = []
-
-def convert_clickout_to_df(row):
-    df_ = pd.DataFrame()
-    df_["item_id"] = row["impressions"].split("|")
-    df_["id"] = row["id"]
-    df_["session_id"] = row["session_id"]
-    df_["user_id"] = row["user_id"]
-    df_["prices"] = list(map(int, row["prices"].split("|")))
-    df_["was_clicked"] = df_["item_id"] == row["reference"]
-    df_["rank"] = np.arange(df_.shape[0])
-    df_["validation"] = row["validation"]
-    df_["platform"] = row["platform"]
-    df_["city"] = row["city"]
-    df_["device"] = row["device"]
-    df_["timestamp"] = row["timestamp"]
-    df_["current_filters"] = row["current_filters"]
-    df_["impressions_before"] = list(map(int, row["impressions_cum_for_item"].split("|")))
-    df_["clicks_before"] = list(map(int, row["clicks_cum_for_item"].split("|")))
-    df_["impressions_user_before"] = list(map(int, row["impressions_cum_for_item_user"].split("|")))
-    df_["clicks_user_before"] = list(map(int, row["clicks_cum_for_item_user"].split("|")))
-    return df_
-
-
-with Pool(8) as pool:
-    dfs = list(pool.imap(convert_clickout_to_df, (row for _, row in tqdm(clickouts.iterrows()))))
-
-df_per_item = pd.concat(dfs, axis=0)
-df_per_item = pd.merge(df_per_item, item_metadata, on="item_id", how="left")
-df_per_item["properties"].fillna("UNK", inplace=True)
-
-split_timestamp = np.percentile(df_per_item.timestamp, 70)
-
-df_per_item["ctr"] = df_per_item["clicks_before"] / (df_per_item["impressions_before"]+1)
-
-df_train = df_per_item[df_per_item["timestamp"] < split_timestamp]
-df_val = df_per_item[(df_per_item["timestamp"] > split_timestamp) & (df_per_item["validation"])]
-
-numerical_features = ["rank", "prices", "impressions_before", "clicks_before", "ctr", "impressions_user_before", "clicks_user_before"]
-categorical_features = ["device", "platform"]
-
-vectorizer = ColumnTransformer([
-    ('numerical', make_pipeline(SimpleImputer(strategy="mean"), StandardScaler()), numerical_features),
-    ('categorical', make_pipeline(SimpleImputer(strategy="most_frequent"), OneHotEncoder()), categorical_features),
-    ('properties', CountVectorizer(preprocessor=lambda x: "UNK" if x!=x else x, tokenizer=lambda x: x.split("|"), min_df=5), "properties")
-])
-
-model = make_pipeline(vectorizer, VotingClassifier([
-    ('lgb', LGBMClassifier(n_estimators=100, n_jobs=-2)),
-    ('mlp1', MLPClassifier(hidden_layer_sizes=(25,), n_iter_no_change=3, verbose=True)),
-    ('mlp2', MLPClassifier(hidden_layer_sizes=(50,), n_iter_no_change=3, verbose=True)),
-    ('mlp3', MLPClassifier(hidden_layer_sizes=(10,10), n_iter_no_change=3, verbose=True))
-], voting="soft"))
-model.fit(df_train, df_train["was_clicked"])
-
-train_pred = model.predict_proba(df_train)[:, 1]
-print(roc_auc_score(df_train["was_clicked"].values, train_pred))
-
-val_pred = model.predict_proba(df_val)[:, 1]
-print(roc_auc_score(df_val["was_clicked"].values, val_pred))
-
-df_val["click_proba"] = val_pred
-
-sessions_items = {}
-for session_id, session_df in tqdm(df_val.groupby("id")):
-    session_df = session_df.sort_values("click_proba", ascending=False)
-    sessions_items[session_id] = session_df.item_id.to_list()
-
-clickouts["predicted_items"] = clickouts["id"].map(sessions_items)
-
-# benchmark
-print("Benchmark")
-print(calculate_mean_rec_err(
-    clickouts[~clickouts["predicted_items"].isnull()]["impressions"].map(lambda x: x.split("|")).to_list(),
-    clickouts[~clickouts["predicted_items"].isnull()]["reference"]))
-
-print("Model")
-print(calculate_mean_rec_err(clickouts[~clickouts["predicted_items"].isnull()]["predicted_items"].to_list(),
-                             clickouts[~clickouts["predicted_items"].isnull()]["reference"]))
+    inp.close()
+    out.close()
