e4ff45f5ab3f5893c17a8d4d45ffb37aa48eefb7
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2019-03-10 13:55:43 +0100

    Training on 10k users, Validation: 0.5005 Leaderboard 0.5267

--- src/recsys/generate_training_data.py
+++ src/recsys/generate_training_data.py
@@ -1,7 +1,21 @@
+import json
 from collections import defaultdict
 from csv import DictReader, DictWriter
 
+import click
 from tqdm import tqdm
+
+ACTION_SHORTENER = {
+    "change of sort order": "a",
+    "clickout item": "b",
+    "filter selection": "c",
+    "interaction item deals": "d",
+    "interaction item image": "e",
+    "interaction item info": "f",
+    "search for destination": "g",
+    "search for item": "h",
+    "search for poi": "i",
+}
 
 
 class StatsAcc:
@@ -28,7 +42,7 @@ class StatsAcc:
             self.updater(self.acc, row)
 
     def get_stats(self, row, item):
-        return self.get_stats_func(self.acc, row, item_id)
+        return self.get_stats_func(self.acc, row, item)
 
 
 def increment_key_by_one(acc, key):
@@ -44,9 +58,76 @@ def increment_keys_by_one(acc, keys):
 
 def set_key(acc, key, value):
     acc[key] = value
+    return True
+
+
+def set_nested_key(acc, key1, key2, value):
+    acc[key1][key2] = value
+    return acc
+
+
+def append_to_list(acc, key, value):
+    acc[key].append(value)
+    return True
+
+
+def diff_ts(acc, current_ts):
+    new_acc = acc.copy()
+    for key in new_acc.keys():
+        new_acc[key] = current_ts - acc[key]
+    return new_acc
 
 
 accumulators = [
+    StatsAcc(
+        name="last_10_actions",
+        filter=lambda row: True,
+        acc=defaultdict(list),
+        updater=lambda acc, row: append_to_list(
+            acc, row["user_id"], ACTION_SHORTENER[row["action_type"]]
+        ),
+        get_stats_func=lambda acc, row, item: "".join(
+            ["q"] + acc[row["user_id"]] + ["x"]
+        ),
+    ),
+    StatsAcc(
+        name="last_sort_order",
+        filter=lambda row: row["action_type"] == "change of sort order",
+        acc={},
+        updater=lambda acc, row: set_key(acc, row["user_id"], row["reference"]),
+        get_stats_func=lambda acc, row, item: acc.get(row["user_id"], "UNK"),
+    ),
+    StatsAcc(
+        name="last_filter_selection",
+        filter=lambda row: row["action_type"] == "filter selection",
+        acc={},
+        updater=lambda acc, row: set_key(acc, row["user_id"], row["reference"]),
+        get_stats_func=lambda acc, row, item: acc.get(row["user_id"], "UNK"),
+    ),
+    StatsAcc(
+        name="last_item_index",
+        filter=lambda row: row["action_type"] == "clickout item",
+        acc={},
+        updater=lambda acc, row: set_key(
+            acc,
+            row["user_id"],
+            row["impressions"].index(row["reference"])
+            if row["reference"] in row["impressions"]
+            else -2,
+        ),
+        get_stats_func=lambda acc, row, item: acc.get(row["user_id"], -1),
+    ),
+    StatsAcc(
+        name="last_event_ts",
+        filter=lambda row: True,
+        acc=defaultdict(lambda: defaultdict(int)),
+        updater=lambda acc, row: set_nested_key(
+            acc, row["user_id"], ACTION_SHORTENER[row["action_type"]], row["timestamp"]
+        ),
+        get_stats_func=lambda acc, row, item: json.dumps(
+            diff_ts(acc[row["user_id"]], row["timestamp"])
+        ),
+    ),
     StatsAcc(
         name="clickout_user_item_clicks",
         filter=lambda row: row["action_type"] == "clickout item",
@@ -55,6 +136,13 @@ accumulators = [
             acc, (row["user_id"], row["reference"])
         ),
         get_stats_func=lambda acc, row, item: acc[(row["user_id"], item)],
+    ),
+    StatsAcc(
+        name="last_item_clickout",
+        filter=lambda row: row["action_type"] == "clickout item",
+        acc={},
+        updater=lambda acc, row: set_key(acc, row["user_id"], row["reference"]),
+        get_stats_func=lambda acc, row, item: acc.get(row["user_id"], 0),
     ),
     StatsAcc(
         name="clickout_item_clicks",
@@ -67,10 +155,17 @@ accumulators = [
         name="clickout_item_impressions",
         filter=lambda row: row["action_type"] == "clickout item",
         acc=defaultdict(int),
-        updater=lambda acc, row: increment_keys_by_one(
-            acc, row["impressions"].split("|")
-        ),
+        updater=lambda acc, row: increment_keys_by_one(acc, row["impressions"]),
         get_stats_func=lambda acc, row, item: acc[item],
+    ),
+    StatsAcc(
+        name="clickout_user_item_impressions",
+        filter=lambda row: row["action_type"] == "clickout item",
+        acc=defaultdict(int),
+        updater=lambda acc, row: increment_keys_by_one(
+            acc, [(row["user_id"], item_id) for item_id in row["impressions"]]
+        ),
+        get_stats_func=lambda acc, row, item: acc[(row["user_id"], item)],
     ),
     StatsAcc(
         name="was_interaction_img",
@@ -120,9 +215,19 @@ accumulators = [
         ),
         get_stats_func=lambda acc, row, item: acc[(row["user_id"], item)],
     ),
+    StatsAcc(
+        name="was_item_searched",
+        filter=lambda row: row["action_type"] == "search for item",
+        acc={},
+        updater=lambda acc, row: set_key(acc, row["user_id"], row["reference"]),
+        get_stats_func=lambda acc, row, item: int(acc.get(row["user_id"]) == item),
+    ),
 ]
 
-if __name__ == "__main__":
+
+@click.command()
+@click.option("--limit", type=int, help="Number of rows to process")
+def main(limit):
     inp = open("../../data/events_sorted.csv")
     dr = DictReader(inp)
     out = open("../../data/events_sorted_trans.csv", "wt")
@@ -130,12 +235,15 @@ if __name__ == "__main__":
     all_obs = []
     first_row = True
     for clickout_id, row in enumerate(tqdm(dr)):
+        if limit and clickout_id > limit:
+            break
         user_id = row["user_id"]
+        row["timestamp"] = int(row["timestamp"])
         if row["action_type"] == "clickout item":
-            item_ids = row["impressions"].split("|")
+            row["impressions"] = row["impressions"].split("|")
             prices = list(map(int, row["prices"].split("|")))
             # create training data
-            for rank, (item_id, price) in enumerate(zip(item_ids, prices)):
+            for rank, (item_id, price) in enumerate(zip(row["impressions"], prices)):
                 obs = row.copy()
                 del obs["impressions"]
                 del obs["prices"]
@@ -161,3 +269,7 @@ if __name__ == "__main__":
 
     inp.close()
     out.close()
+
+
+if __name__ == "__main__":
+    main()
--- src/recsys/transformers.py
+++ src/recsys/transformers.py
@@ -1,14 +1,36 @@
-from recsys.utils import timer
+import joblib
+import numpy as np
+import pandas as pd
+from recsys.utils import jaccard
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class FeatureEng(BaseEstimator, TransformerMixin):
+    features = [
+        "item_id",
+        "last_item_clickout",
+        "clickout_user_item_clicks",
+        "clickout_user_item_impressions",
+    ]
+
+    def __init__(self):
+        self.imm = joblib.load("../../data/item_metadata_map.joblib")
+        self.item_metadata = pd.read_csv("../../data/item_metadata.csv")
+
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        X["ctr"] = X["clickout_item_clicks"] / (X["clickout_item_impressions"] + 1)
-        return X[["ctr"]]
+        X["item_similarity_to_last_clicked_item"] = X.apply(
+            lambda row: jaccard(self.imm[row["item_id"]], self.imm[row["last_item_clickout"]]),
+            axis=1,
+        )
+        X["user_item_ctr"] = X["clickout_user_item_clicks"] / (
+                X["clickout_user_item_impressions"] + 1
+        )
+        X = pd.merge(X, self.item_metadata, on="item_id", how="left")
+        X["properties"].fillna("", inplace=True)
+        return X
 
 
 class RankFeatures(BaseEstimator, TransformerMixin):
@@ -28,3 +50,11 @@ class PandasToRecords(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         return X.to_dict(orient="records")
+
+
+class PandasToNpArray(BaseEstimator, TransformerMixin):
+    def fit(self, X, *arg):
+        return self
+
+    def transform(self, X):
+        return X.values.astype(np.float)
