6a50fe76b76808891c4f67d6f82a1215d2f415ca
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2019-03-30 12:18:17 +0100

    Add more offsets

--- src/recsys/transformers.py
+++ src/recsys/transformers.py
@@ -1,5 +1,7 @@
 import json
 import pathlib
+from collections import Counter
+from typing import List
 
 import arrow
 import joblib
@@ -59,13 +61,16 @@ class FeatureEngScala(BaseEstimator, TransformerMixin):
 
 
 class RankFeatures(BaseEstimator, TransformerMixin):
+    def __init__(self, ascending=False):
+        self.ascending = ascending
+
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
         for col in X.columns:
             if col != "clickout_id":
-                X[col] = X.groupby("clickout_id")[col].rank("max", ascending=False) - 1
+                X[col] = X.groupby("clickout_id")[col].rank("max", ascending=self.ascending) - 1
         X.drop("clickout_id", axis=1, inplace=True)
         return X
 
@@ -77,10 +82,11 @@ class LagNumericalFeaturesWithinGroup(BaseEstimator, TransformerMixin):
     def transform(self, X):
         for col in X.columns:
             if col != "clickout_id":
-                X[col + "_shifted_p1_diff"] = X[col] - X.groupby(["clickout_id"])[col].shift(1).fillna(0)
-                X[col + "_shifted_m1_diff"] = X[col] - X.groupby(["clickout_id"])[col].shift(-1).fillna(0)
-                X[col + "_shifted_p1"] = X.groupby(["clickout_id"])[col].shift(1).fillna(0)
-                X[col + "_shifted_m1"] = X.groupby(["clickout_id"])[col].shift(-1).fillna(0)
+                for n in [1, 2]:
+                    X[col + f"_shifted_p{n}_diff"] = X[col] - X.groupby(["clickout_id"])[col].shift(n).fillna(0)
+                    X[col + f"_shifted_m{n}_diff"] = X[col] - X.groupby(["clickout_id"])[col].shift(-n).fillna(0)
+                    X[col + f"_shifted_p{n}"] = X.groupby(["clickout_id"])[col].shift(n).fillna(0)
+                    X[col + f"_shifted_m{n}"] = X.groupby(["clickout_id"])[col].shift(-n).fillna(0)
         X.drop("clickout_id", axis=1, inplace=True)
         return X
 
@@ -118,6 +124,19 @@ class PandasToNpArray(BaseEstimator, TransformerMixin):
         return X.values.astype(np.float)
 
 
+class WrapPandas(BaseEstimator, TransformerMixin):
+    def __init__(self, trans):
+        self.trans = trans
+
+    def fit(self, *args):
+        self.trans.fit(*args)
+        return self
+
+    def transform(self, X):
+        X_ = self.trans.transform(X)
+        return pd.DataFrame(X_, columns=self.trans.get_feature_names())
+
+
 class MinimizeNNZ(BaseEstimator, TransformerMixin):
     """
     Offset the values so that the most frequent is offset to 0 if it is the most common one
@@ -141,3 +160,76 @@ class MinimizeNNZ(BaseEstimator, TransformerMixin):
             if offset != 0:
                 X[col] += offset
         return X
+
+
+class ClickTrackerEng(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, *arg):
+        return self
+
+    @staticmethod
+    def classify_sequence(seq):
+        if len(seq) == 0:
+            return "empty"
+        elif len(set(seq)) == 1:
+            return "constant"
+        elif len(set(seq)) == len(seq) and min(seq) == seq[0] and max(seq) == seq[-1]:
+            return "ideal sequence"
+        elif seq == sorted(seq):
+            return "non ideal sequence"
+        elif len(set(seq)) == len(seq) and min(seq) == seq[-1] and max(seq) == seq[0]:
+            return "ideal sequence rev"
+        elif seq == sorted(seq, reverse=True):
+            return "non ideal sequence rev"
+        else:
+            return "other"
+
+    def transform(self, X):
+        ranks = list(X["rank"].values)
+        clickout_ids = list(X["clickout_id"].values)
+        users_sessions: List[List[List[int]]] = [json.loads(el) for el in X['click_indices']]
+        all_features = []
+        for user_sessions, rank, clickout_id in zip(users_sessions, ranks, clickout_ids):
+            user_features = self.extract_features_from_user_sessions(rank, user_sessions)
+            user_features['clickout_id'] = clickout_id
+            all_features.append(user_features)
+        return all_features
+
+    def extract_features_from_user_sessions(self, rank, user_sessions):
+        user_features = {}
+        classes = [self.classify_sequence(sess) for sess in user_sessions]
+        if classes:
+            last_session = user_sessions[-1]
+            last_class = classes[-1]
+            last_class_with_new_item = self.classify_sequence(last_session + [rank])
+            if self.continuation(last_class, last_class_with_new_item):
+                user_features['sequence_{}_continuation'.format(last_class_with_new_item)] = len(last_session)
+            user_features['sequence_{}_len'.format(last_class)] = len(last_session)
+        for cl, freq in Counter(classes).most_common():
+            user_features['sequence_{}_freq'.format(cl)] = freq
+        return user_features
+
+    def continuation(self, last_class, last_class_with_new_item):
+        """
+        Check if there is a continuation of a sequence
+        If a sequence is ideal and is continued then it is ok
+        But also when the previous class is ideal then there is a possibility that the class will be not ideal
+        but still sorted. The same is for constant -> non ideal sequence
+        """
+        if last_class_with_new_item == last_class:
+            return True
+        elif last_class_with_new_item.startswith("non ideal sequence") and last_class.startswith("ideal sequence"):
+            return True
+        elif last_class == "constant" and "sequence" in last_class_with_new_item:
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    click = ClickTrackerEng()
+    print(click.extract_features_from_user_sessions(3, [[3, 2, 1], [1, 2, 3], [0, 0, 0]]))
+    print(click.extract_features_from_user_sessions(0, [[0, 0, 0]]))
+    print(click.extract_features_from_user_sessions(3, [[1, 2, 3]]))
+    print(click.extract_features_from_user_sessions(4, [[1, 2, 3]]))
+    print(click.extract_features_from_user_sessions(4, [[0, 0, 0], [1, 1, 1], [1, 2, 3]]))
