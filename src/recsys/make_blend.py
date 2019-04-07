28cfd3beb90f7aee935375726e6dbb09fd24dc64
Paweł Jankiewicz <pawel@logicai.io>
2019-04-07 21:37:19 +0000

    Fix logging

--- src/recsys/make_blend.py
+++ src/recsys/make_blend.py
@@ -11,7 +11,7 @@ p_lgbr = pd.read_csv("predictions/model_2_val.csv")
 final = p_lgbr.copy()
 final["click_proba"] = 1.0 * p_lgbr["click_proba"]
 mrr = mrr_fast(final, "click_proba")
-logger.info("MRR", mrr)
+logger.info(f"MRR {mrr}")
 
 p_lgbr = pd.read_csv("predictions/model_2_submit.csv")
 final = p_lgbr.copy()
