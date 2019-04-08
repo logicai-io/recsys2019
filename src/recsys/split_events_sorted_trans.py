0d1c72b14bc2e9b26dd5308765a965c204f62b2b
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2019-04-09 00:24:15 +0200

    Fix chunking

--- src/recsys/split_events_sorted_trans.py
+++ src/recsys/split_events_sorted_trans.py
@@ -10,32 +10,31 @@ logger.info("Starting splitting")
 header = []
 
 # split the data so that train/val/test are continguous
-outputs = [
+outputs_tr = [
     DictWriter(open("../../data/proc/raw_csv/01_train_{:04d}.csv".format(chunk_id), "wt"), fieldnames=header)
     for chunk_id in range(20)
-] + [
-    DictWriter(open("../../data/proc/raw_csv/02_val_{:04d}.csv".format(chunk_id), "wt"), fieldnames=header)
-    for chunk_id in range(1)
-] + [
+]
+outputs_va= [DictWriter(open("../../data/proc/raw_csv/02_val_0001.csv", "wt"), fieldnames=header)]
+outputs_te = [
     DictWriter(open("../../data/proc/raw_csv/03_test_{:04d}.csv".format(chunk_id), "wt"), fieldnames=header)
-    for chunk_id in range(9)
+    for chunk_id in range(10)
 ]
 
 reader = DictReader(open("../../data/events_sorted_trans.csv"))
 
 for i, row in tqdm(enumerate(reader)):
     if i == 0:
-        for output in outputs:
+        for output in outputs_tr + outputs_va + outputs_te:
             output.fieldnames = row.keys()
             output.writeheader()
     if (row["is_val"] == "0") and (row["is_test"] == "0"):
         find = int(row["clickout_id"]) % 20
-        outputs[find].writerow(row)
+        outputs_tr[find].writerow(row)
     elif (row["is_val"] == "1") and (row["is_test"] == "0"):
-        outputs[20].writerow(row)
+        outputs_va[0].writerow(row)
     elif (row["is_test"] == "1"):
-        find = 20 + int(row["clickout_id"]) % 10
-        outputs[find].writerow(row)
+        find = int(row["clickout_id"]) % 10
+        outputs_te[find].writerow(row)
     else:
         raise ValueError("Shouldn't happen")
 
