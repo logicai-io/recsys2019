bab10b41eabe7c05ac5a37ebc8aa850cf045daf4
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2019-04-08 23:35:47 +0200

    Make chunks contiguous wrt train/val/test

--- src/recsys/split_events_sorted_trans.py
+++ src/recsys/split_events_sorted_trans.py
@@ -9,9 +9,16 @@ logger.info("Starting splitting")
 
 header = []
 
+# split the data so that train/val/test are continguous
 outputs = [
-    DictWriter(open("../../data/proc/raw_csv/{:04d}.csv".format(chunk_id), "wt"), fieldnames=header)
-    for chunk_id in range(30)
+    DictWriter(open("../../data/proc/raw_csv/01_train_{:04d}.csv".format(chunk_id), "wt"), fieldnames=header)
+    for chunk_id in range(20)
+] + [
+    DictWriter(open("../../data/proc/raw_csv/02_val_{:04d}.csv".format(chunk_id), "wt"), fieldnames=header)
+    for chunk_id in range(1)
+] + [
+    DictWriter(open("../../data/proc/raw_csv/03_test_{:04d}.csv".format(chunk_id), "wt"), fieldnames=header)
+    for chunk_id in range(9)
 ]
 
 reader = DictReader(open("../../data/events_sorted_trans.csv"))
@@ -21,7 +28,15 @@ for i, row in tqdm(enumerate(reader)):
         for output in outputs:
             output.fieldnames = row.keys()
             output.writeheader()
-    find = int(row["clickout_id"]) % 30
-    outputs[find].writerow(row)
+    if (row["is_val"] == "0") and (row["is_test"] == "0"):
+        find = int(row["clickout_id"]) % 20
+        outputs[find].writerow(row)
+    elif (row["is_val"] == "1") and (row["is_test"] == "0"):
+        outputs[20].writerow(row)
+    elif (row["is_test"] == "1"):
+        find = 20 + int(row["clickout_id"]) % 10
+        outputs[find].writerow(row)
+    else:
+        raise ValueError("Shouldn't happen")
 
 logger.info("Stop splitting")
