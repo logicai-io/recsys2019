df895b17098e9b6550ef8b8565307b0a2637f875
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2019-04-09 05:57:55 -0700

    Improve chunking (#33)
    * Make chunks contiguous wrt train/val/test
    
    * Fix chunking
    
    * Change split sizes
    
    * Lazy load sparse matrix

--- src/recsys/split_events_sorted_trans.py
+++ src/recsys/split_events_sorted_trans.py
@@ -9,19 +9,33 @@ logger.info("Starting splitting")
 
 header = []
 
-outputs = [
-    DictWriter(open("../../data/proc/raw_csv/{:04d}.csv".format(chunk_id), "wt"), fieldnames=header)
-    for chunk_id in range(30)
+# split the data so that train/val/test are continguous
+outputs_tr = [
+    DictWriter(open("../../data/proc/raw_csv/01_train_{:04d}.csv".format(chunk_id), "wt"), fieldnames=header)
+    for chunk_id in range(25)
+]
+outputs_va= [DictWriter(open("../../data/proc/raw_csv/02_val_0001.csv", "wt"), fieldnames=header)]
+outputs_te = [
+    DictWriter(open("../../data/proc/raw_csv/03_test_{:04d}.csv".format(chunk_id), "wt"), fieldnames=header)
+    for chunk_id in range(4)
 ]
 
 reader = DictReader(open("../../data/events_sorted_trans.csv"))
 
 for i, row in tqdm(enumerate(reader)):
     if i == 0:
-        for output in outputs:
+        for output in outputs_tr + outputs_va + outputs_te:
             output.fieldnames = row.keys()
             output.writeheader()
-    find = int(row["clickout_id"]) % 30
-    outputs[find].writerow(row)
+    if (row["is_val"] == "0") and (row["is_test"] == "0"):
+        find = int(row["clickout_id"]) % 25
+        outputs_tr[find].writerow(row)
+    elif (row["is_val"] == "1") and (row["is_test"] == "0"):
+        outputs_va[0].writerow(row)
+    elif (row["is_test"] == "1"):
+        find = int(row["clickout_id"]) % 4
+        outputs_te[find].writerow(row)
+    else:
+        raise ValueError("Shouldn't happen")
 
 logger.info("Stop splitting")
