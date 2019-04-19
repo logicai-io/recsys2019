007626590d51fb1053f2c10954ed8b329bc7851c
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2019-04-19 21:16:01 +0200

    Split accumulator helpers

--- src/recsys/accumulators.py
+++ src/recsys/accumulators.py
@@ -2,6 +2,19 @@ import json
 from collections import defaultdict
 
 import joblib
+from recsys.accumulators_helpers import (
+    increment_key_by_one,
+    increment_keys_by_one,
+    add_to_set,
+    set_key,
+    set_nested_key,
+    add_one_nested_key,
+    append_to_list,
+    append_to_list_not_null,
+    diff_ts,
+    tryint,
+    diff,
+)
 from recsys.jaccard_sim import JaccardItemSim, ItemPriceSim
 from recsys.log_utils import get_logger
 from recsys.utils import group_time
@@ -285,69 +298,6 @@ class SimilarityFeatures:
         )
         output["num_pois"] = len(self.poi_sim.imm[item_id])
         return output
-
-
-def increment_key_by_one(acc, key):
-    acc[key] += 1
-    return acc
-
-
-def increment_keys_by_one(acc, keys):
-    for key in keys:
-        acc[key] += 1
-    return acc
-
-
-def add_to_set(acc, key, value):
-    acc[key].add(value)
-    return True
-
-
-def set_key(acc, key, value):
-    acc[key] = value
-    return True
-
-
-def set_nested_key(acc, key1, key2, value):
-    acc[key1][key2] = value
-    return acc
-
-
-def add_one_nested_key(acc, key1, key2):
-    acc[key1][key2] += 1
-    return acc
-
-
-def append_to_list(acc, key, value):
-    acc[key].append(value)
-    return True
-
-
-def append_to_list_not_null(acc, key, value):
-    if value:
-        acc[key].append(value)
-    return True
-
-
-def diff_ts(acc, current_ts):
-    new_acc = acc.copy()
-    for key in new_acc.keys():
-        new_acc[key] = current_ts - acc[key]
-    return new_acc
-
-
-def tryint(s):
-    try:
-        return int(s)
-    except:
-        return 0
-
-
-def diff(seq):
-    new_seq = []
-    for n in range(len(seq) - 1):
-        new_seq.append(seq[n + 1] - seq[n])
-    return new_seq
 
 
 def get_accumulators():
