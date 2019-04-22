9d8f756b3f2309ccf4551861bbe36b955b1a7852
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2019-04-23 00:44:09 +0200

    Add partition_id when joining the datasets

--- src/recsys/data_prep/join_datasets.py
+++ src/recsys/data_prep/join_datasets.py
@@ -1,5 +1,26 @@
+import hashlib
+
 import numpy as np
 import pandas as pd
+
+
+def hash_str(text):
+    return int(hashlib.sha1(text.encode("utf-8")).hexdigest(), 16) % (10 ** 8)
+
+
+def partition_row(row):
+    hashn = hash_str(row["user_id"])
+    if (row["is_val"] == 0) and (row["is_test"] == 0):
+        find = hashn % 25
+        return "01_train_%04d" % find
+    elif (row["is_val"] == 1) and (row["is_test"] == 0):
+        return "02_val_%04d" % 0
+    elif row["is_test"] == 1:
+        find = hashn % 4
+        return "03_test_%04d" % find
+    else:
+        raise ValueError('No matching value to split row')
+
 
 train = pd.read_csv("../../../data/train.csv")
 train["src"] = "train"
@@ -17,9 +38,10 @@ test["is_val"].fillna(0, inplace=True)
 
 assert np.all(train.columns == test.columns)
 
-events = pd.concat([train, test], axis=0)
+events: pd.DataFrame = pd.concat([train, test], axis=0)
 events["is_val"] = events["is_val"].astype(np.int)
 events.sort_values(["timestamp", "user_id", "step"], inplace=True)
 events["fake_impressions"] = events.groupby(["user_id", "session_id"])["impressions"].bfill()
 events["fake_prices"] = events.groupby(["user_id", "session_id"])["prices"].bfill()
+events.insert(0, "partition_id", events.apply(partition_row, axis=1))
 events.to_csv("../../../data/events_sorted.csv", index=False)
