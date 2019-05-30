05bf576559cb09d9f6bafa71379f24cfca62ba89
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2019-05-30 11:00:52 +0200

    Add scripts to data_prep

--- src/recsys/data_prep/run_data_prep.sh
+++ src/recsys/data_prep/run_data_prep.sh
@@ -2,5 +2,7 @@ python join_datasets.py
 python convert_item_metadata_to_sets.py
 python extract_hotel_dense_features.py
 python extract_item_prices.py
+python extract_item_prices_rank.py
 python generate_click_indices.py
 python assign_poi_to_items.py
+python extract_city_prices_percentiles.py
