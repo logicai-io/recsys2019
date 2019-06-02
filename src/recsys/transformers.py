dd00837151b64e5e726b3816d1f023012570fe8d
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2019-06-02 20:50:47 +0200

    Fix join

--- src/recsys/transformers.py
+++ src/recsys/transformers.py
@@ -69,10 +69,10 @@ class FeatureEng(BaseEstimator, TransformerMixin):
         X = pd.merge(X, price_rank, how="left", on=["item_id", "price"])
 
         lstm_user_session = pd.read_csv(LSTM_USER_SESSION).rename(columns={'prob': 'lstm_user_session_prob'})
-        X = pd.merge(X, lstm_user_session, how="left", on=["user_id", "session_id", "step"])
+        X = pd.merge(X, lstm_user_session, how="left", on=["user_id", "session_id", "item_id", "step"])
 
         lstm_user = pd.read_csv(LSTM_USER).rename(columns={'prob': 'lstm_user_prob'})
-        X = pd.merge(X, lstm_user, how="left", on=["user_id", "session_id", "step"])
+        X = pd.merge(X, lstm_user, how="left", on=["user_id", "session_id", "item_id", "step"])
 
         for col in X.columns:
             if X[col].dtype == np.bool:
